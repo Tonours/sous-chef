7bf74fe90cc8b90ba4f526237f30dfd30f3daab8
Philippe Mouchel <philippe.mouchel@savoirfairelinux.com>
2016-08-12 12:47:57 -0400

    Transform processed column into operation_status
    to be able to store more than two values : tobeprocessed (new), processed and error
    issue #313

--- django/santropolFeast/member/models.py
+++ django/santropolFeast/member/models.py
@@ -601,6 +601,26 @@ class Client(models.Model):
 
 class Client_scheduled_status(models.Model):
 
+    ALONE = 'ALONE'
+    START = 'START'
+    END = 'END'
+
+    CHANGE_STATUS = (
+        (ALONE, _('Alone')),
+        (START, _('Start')),
+        (END, _('End')),
+    )
+
+    TOBEPROCESSED = 'NEW'
+    PROCESSED = 'PRO'
+    ERROR = 'ERR'
+
+    OPERATION_STATUS = (
+        (TOBEPROCESSED, _('To be processed')),
+        (PROCESSED, _('Processed')),
+        (ERROR, _('Error')),
+    )
+
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     linked_scheduled_status = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, default = None)
     status_from = models.CharField(max_length=1, choices=Client.CLIENT_STATUS)
@@ -608,8 +628,8 @@ class Client_scheduled_status(models.Model):
     reason = models.CharField(max_length=200, blank=True, default = '')
     change_date = models.DateField(auto_now=False, auto_now_add=False,
         default=timezone.now, blank=True, null=True)
-    change_state = models.CharField(max_length=5, choices=(('alone', 'Alone'), ('start', 'Start'), ('end', 'End')))
-    processed = models.BooleanField(default=False)
+    change_state = models.CharField(max_length=5, choices=CHANGE_STATUS, default=ALONE)
+    operation_status = models.CharField(max_length=3, choices=OPERATION_STATUS, default=TOBEPROCESSED)
 
     def __str__(self):
         return "{}: from {} to {}, on {}".format(self.client.member, self.status_from, self.status_to, self.change_date)
